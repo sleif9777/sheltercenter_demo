450d589efa8e40e660612b18d7658ec28d9c65ce
Sam <leifersam9@gmail.com>
2021-12-24 17:03:47 -0600

    static root off

--- demo/settings.py
+++ demo/settings.py
@@ -162,12 +162,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
-if not DEBUG:
-    STATIC_ROOT = ''
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/'),
-]
+# if not DEBUG:
+#     STATIC_ROOT = ''
+#
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static/'),
+# ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
