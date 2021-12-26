106ddeb1e31cd6d3b235d67d4288642a7bb61a0b
Sam <leifersam9@gmail.com>
2021-12-25 18:08:53 -0600

    reset

--- demo/settings.py
+++ demo/settings.py
@@ -165,9 +165,9 @@ STATIC_URL = '/static/'
 # if not DEBUG:
 #     STATIC_ROOT = ''
 #
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static/'),
+# ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
