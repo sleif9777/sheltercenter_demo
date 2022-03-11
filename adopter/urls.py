bd6fcf9560105d707f575a7a6c8883cd83ba8d59
Sam <leifersam9@gmail.com>
2022-03-10 22:35:43 -0600

    url update for adopter

--- adopter/urls.py
+++ adopter/urls.py
@@ -16,5 +16,5 @@ urlpatterns = [
     path('simple_add/submit/', views.simple_add_form_submit, name="saf_submit"),
     path('simple_add/oos/', views.simple_add_form_oos, name="saf_sub_oos"),
     path('edit/<int:adopter_id>/', views.edit_adopter, name="edit_adopter"),
-    path('manage/', views.manage, name="adopter_manage")
+    path('manage/', views.manage, name="adopter_manage"),
 ]
