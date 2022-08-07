f4afef07567d34e77808ee098292b5ef63ddb2d7
Sam Leifer <samleifer@Sams-Air.attlocal.net>
2022-08-07 10:10:31 -0500

    wishlist changes

--- wishlist/views.py
+++ wishlist/views.py
@@ -50,7 +50,7 @@ def check_for_updated_status():
 @authenticated_user
 @allowed_users(allowed_roles={'superuser'})
 def display_list(request):
-    get_and_update_dogs()
+    # get_and_update_dogs()
 
     all_available_dogs = [dog for dog in Dog.objects.filter(shelterluv_status = 'Available for Adoption').order_by('name')]
 
