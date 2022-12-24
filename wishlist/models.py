edc2e70538c203a50f29857df7cef78efa420ee0
Sam Leifer <samleifer@Sams-Air.attlocal.net>
2022-12-24 12:30:04 -0600

    begin wishlist

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,8 +1,11 @@
+import datetime
+
 from django.db import models
 
 class Dog(models.Model):
-    shelterluv_id = models.CharField(default = "", max_length = 200)
-    shelterluv_status = models.CharField(default = "Available for Adoption", max_length = 200)
+    shelterluv_id = models.CharField(default="", max_length = 200)
+    name = models.CharField(default="", null=True, max_length=20)
+    shelterluv_status = models.CharField(default="Available for Adoption", max_length=200)
     info = models.JSONField()
 
     def __repr__(self):
@@ -22,3 +25,45 @@ class Dog(models.Model):
             return "{0} lbs., ".format(weight)
         except:
             return ""
+
+class DogProfile(models.Model):
+    name = models.CharField(default = "", max_length = 200)
+    shelterluv_id = models.CharField(default = "", max_length = 200)
+    shelterluv_status = models.CharField(default = "Available for Adoption", max_length = 200)
+    info = models.JSONField()
+    offsite = models.BooleanField(default=False)
+    appt_only = models.BooleanField(default=False)
+    host_date = models.DateField(default=datetime.date(2000,1,1), blank=True)
+    foster_date = models.DateField(default=datetime.date(2000,1,1), blank=True)
+
+    def __repr__(self):
+        return self.info['Name']
+
+    def __str__(self):
+        return self.info['Name']
+
+    def Age_str(self):
+        age = int(self.info['Age'])
+        return "Age {0}Y {1}M".format(age // 12, age % 12)
+
+    def Weight_str(self):
+        try:
+            weight = int(self.info['CurrentWeightPounds'])
+            return "{0} lbs., ".format(weight)
+        except:
+            return ""
+
+    def host_date_str(self):
+        if self.host_date.year != 2000:
+            return self.host_date.strftime("%Y-%m-%d")
+        else:
+            return
+
+    def foster_date_str(self):
+        if self.foster_date.year != 2000:
+            return self.foster_date.strftime("%Y-%m-%d")
+        else:
+            return
+
+    class Meta:
+        ordering = ('name', 'id',)
--- wishlist/views.py
+++ wishlist/views.py
@@ -19,28 +19,37 @@ def get_and_update_dogs():
 
     while has_more:
         dogs_request = requests.get('https://www.shelterluv.com/api/v1/animals?offset={0}&status_type=publishable'.format(offset), headers=headers).json()
-
         dogs += dogs_request['animals']
-
         offset += 100
         has_more = dogs_request['has_more']
 
-    for dog in dogs:
-        try:
-            existing_dog = Dog.objects.get(shelterluv_id=dog['Internal-ID'])
-            existing_dog.info = dog
-            existing_dog.shelterluv_status = dog['Status']
-            existing_dog.save()
-        except Exception as e:
-            print('e2', e)
-            new_dog = Dog()
-            new_dog.shelterluv_id = dog['Internal-ID']
-            new_dog.shelterluv_status = dog['Status']
-            new_dog.info = dog
-            new_dog.save()
+    for dog_json in dogs:
+        print(dog_json)
+        
+        dog = DogProfile.objects.update_or_create(
+            shelterluv_id = dog_json['Internal-ID'],
+            defaults={
+                'name': dog_json['Name'],
+                'info': dog_json,
+                'shelterluv_status': dog_json['Status']
+            }
+        )
+
+        # try:
+        #     existing_dog = Dog.objects.get(shelterluv_id=dog['Internal-ID'])
+        #     existing_dog.info = dog
+        #     existing_dog.shelterluv_status = dog['Status']
+        #     existing_dog.save()
+        # except Exception as e:
+        #     print('e2', e)
+        #     new_dog = Dog()
+        #     new_dog.shelterluv_id = dog['Internal-ID']
+        #     new_dog.shelterluv_status = dog['Status']
+        #     new_dog.info = dog
+        #     new_dog.save()
 
 def check_for_updated_status():
-    available_dogs = Dog.objects.get(shelterluv_status="Available for Adoption")
+    available_dogs = DogProfile.objects.get(shelterluv_status="Available for Adoption")
 
     for dog in available_dogs:
         update_request = requests.get('https://www.shelterluv.com/api/v1/animals?offset={0}&status_type=publishable'.format(offset), params=params, headers=headers).json()
@@ -48,12 +57,13 @@ def check_for_updated_status():
 @authenticated_user
 @allowed_users(allowed_roles={'superuser'})
 def display_list(request):
-    get_and_update_dogs()
+    # get_and_update_dogs()
 
-    user_wishlist = {dog for dog in request.user.adopter.wishlist.iterator()}
-    all_available_dogs = {dog for dog in Dog.objects.filter(shelterluv_status = 'Available for Adoption').order_by('shelterluv_id')}
+    # user_wishlist = {dog for dog in request.user.adopter.wishlist.iterator()}
+    all_available_dogs = {dog for dog in DogProfile.objects.filter(shelterluv_status='Available for Adoption').order_by('name')}
+    print([dog.name for dog in all_available_dogs])
 
-    other_available_dogs = all_available_dogs - user_wishlist
+    # other_available_dogs = all_available_dogs - user_wishlist
 
     if request.method == "POST":
         form_data = dict(request.POST)
@@ -61,7 +71,7 @@ def display_list(request):
 
         for id in form_data:
             print(id)
-            user_wishlist.add(Dog.objects.get(shelterluv_id=id))
+            # user_wishlist.add(DogProfile.objects.get(shelterluv_id=id))
 
         print(request.user.adopter.wishlist)
         # for dog in all_available_dogs:
@@ -69,7 +79,7 @@ def display_list(request):
 
     context = {
         'all_available_dogs': all_available_dogs,
-        'user_wishlist': user_wishlist,
+        # 'user_wishlist': user_wishlist,
     }
 
     return render(request, "wishlist/list.html", context)
