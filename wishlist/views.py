2f03ac4414a1e2c14b71d5a835b88b3df2a60f05
sleif9777 <93562334+sleif9777@users.noreply.github.com>
2023-08-17 15:02:34 -0500

    Update views.py

--- wishlist/views.py
+++ wishlist/views.py
@@ -97,7 +97,7 @@ def update_from_shelterluv():
 
 
 def get_timestamp_from_json(dog_json):
-    if dog_json['LastUpdatedUnixTime']:
+    if dog_json.get('LastUpdatedUnixTime'):
         return datetime.datetime.fromtimestamp(
                 int(dog_json['LastUpdatedUnixTime']))
     else:
